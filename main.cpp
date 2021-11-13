d386907c51bbe93828bba20648f785e579178a65
Kalsep <61381739+RealKalsep@users.noreply.github.com>
2021-11-13 15:46:01 +0100

    removed namespace std from main

--- main.cpp
+++ main.cpp
@@ -1,25 +1,24 @@
 ﻿#include <iostream>
 #include "grid.h"
-using namespace std;
 
 int tempWidth, tempHeight;
 //char key;
 
 void gridWidth() {
-	cout << "Grid width (min 2, max 50): ";
-	cin >> tempWidth;
+	std::cout << "Grid width (min 2, max 50): ";
+	std::cin >> tempWidth;
 	if (tempWidth < 2 || tempWidth > 50) {
-		cout << "Invalid size!" << endl;
+		std::cout << "Invalid size!" << std::endl;
 		gridWidth();
 	}
 	width = tempWidth;
 }
 
 void gridHeight() {
-	cout << "Grid height (min 2, max 50): ";
-	cin >> tempHeight;
+	std::cout << "Grid height (min 2, max 50): ";
+	std::cin >> tempHeight;
 	if (tempHeight < 2 || tempHeight > 50) {
-		cout << "Invalid size!" << endl;
+		std::cout << "Invalid size!" << std::endl;
 		gridHeight();
 	}
 	height = tempHeight;
